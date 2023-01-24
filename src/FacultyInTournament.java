35d87fad1b27474ab3268700cfac52dceca48855
GurKeinan <gur.keinan@campus.technion.ac.il>
2023-01-24 15:08:32 +0200

    deleting unneccesary

--- src/FacultyInTournament.java
+++ src/FacultyInTournament.java
@@ -37,41 +37,4 @@ public class FacultyInTournament {
         }
         return max.player;
     }
-
-
-
-
-}
-
-    }
-
-    public PlayerInTournament[] getPlayers() {
-        return players;
-    }
-
-    public void updateScoreBy(int score) {
-        this.score += score;
-    }
-
-    public void setPlayers(PlayerInTournament[] players) {
-        this.players = players;
-    }
-
-    public Player findMax()
-    {
-        PlayerInTournament[] players = this.players;
-        PlayerInTournament max = players[0];
-        for(int i = 1 ; i < 11 && players[i] != null ; i++)
-        {
-            if (players[i].goals > max.goals)
-            {
-                max = players[i];
-            }
-        }
-        return max.player;
-    }
-
-
-
-
 }
